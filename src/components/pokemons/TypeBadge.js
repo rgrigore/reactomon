1f1cd40504ea9af324aab17ffe63f978ce92fad5
Razvan Grigore <razvang95@gmail.com>
2020-12-12 21:38:54 +0200

    TypeBadge

--- src/components/pokemons/TypeBadge.js
+++ src/components/pokemons/TypeBadge.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import TypeColor from '../types/TypeColor';
 
-export default class TypeBadge extends Component {
-	render() {
-		return (
-			<span className="badge badge-pill text-capitalize mr-1" style={{ backgroundColor: TypeColor[this.props.type], color: "white" }}>
-				{ this.props.type }
-			</span>
-		)
-	}
+const TypeBadge = props => {
+	return (
+		<span className="badge badge-pill text-capitalize mr-1" style={{ backgroundColor: TypeColor[props.type], color: "white" }}>
+			{ props.type }
+		</span>
+	)
 }
 
 TypeBadge.propTypes = {
 	type: PropTypes.string.isRequired
 }
+
+export default TypeBadge;
