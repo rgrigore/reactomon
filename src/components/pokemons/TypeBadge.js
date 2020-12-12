6e117a3903a4d359debf0ecd25f9349f77465fe5
Razvan Grigore <razvang95@gmail.com>
2020-12-12 02:21:29 +0200

    Refactor type colors

--- src/components/pokemons/TypeBadge.js
+++ src/components/pokemons/TypeBadge.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const color = {
-	normal: "#aa9",
-	fire: "#f42",
-	water: "#39f",
-	electric: "#fc3",
-	grass: "#7c5",
-	ice: "#6cf",
-	fighting: "#b54",
-	poison: "#a59",
-	ground: "#db5",
-	flying: "#89f",
-	psychic: "#f59",
-	bug: "#ab2",
-	rock: "#ba6",
-	ghost: "#66b",
-	dragon: "#76e",
-	dark: "#754",
-	steel: "#aab",
-	fairy: "#e9e"
-}
+import TypeColor from '../types/TypeColor';
 
 export default class TypeBadge extends Component {
 	render() {
 		return (
-			<span className="badge badge-pill text-capitalize mr-1" style={{ backgroundColor: color[this.props.type], color: "white" }}>
+			<span className="badge badge-pill text-capitalize mr-1" style={{ backgroundColor: TypeColor[this.props.type], color: "white" }}>
 				{ this.props.type }
 			</span>
 		)
---
+++ src/components/types/TypeColor.js
@@ -0,0 +1,22 @@
+export const TypeColor = {
+	normal: "#aa9",
+	fire: "#f42",
+	water: "#39f",
+	electric: "#fc3",
+	grass: "#7c5",
+	ice: "#6cf",
+	fighting: "#b54",
+	poison: "#a59",
+	ground: "#db5",
+	flying: "#89f",
+	psychic: "#f59",
+	bug: "#ab2",
+	rock: "#ba6",
+	ghost: "#66b",
+	dragon: "#76e",
+	dark: "#754",
+	steel: "#aab",
+	fairy: "#e9e"
+};
+
+export default TypeColor;
