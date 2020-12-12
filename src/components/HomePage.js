e951b648658ddce137f0f24b168868e1e7252fb0
Razvan Grigore <razvang95@gmail.com>
2020-12-12 03:01:57 +0200

    Add home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 export default class HomePage extends Component {
 	render() {
 		return (
-			<div>
-				Home
+			<div className="container-fluid">
+				<div className="d-flex justify-content-center">
+					<h1 className="mt-5 pt-5 font-weight-bold">Welcome to Reactomon</h1>
+				</div>
 			</div>
 		)
 	}
